db955fb674f54f0c54f0d719071caa58b0450af4
tang <tanguy.bonneau@gmail.com>
2021-06-01 14:42:51 +0200

    Update install

--- scripts/postinst.sh
+++ scripts/postinst.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # install libs
-python3 -m pip install --trusted-host pypi.org "pyre-gevent==0.2.3" "pyzmq==22.0.3"
+python3 -m pip install --trusted-host pypi.org "pyre-gevent==0.2.3" "pyzmq==22.0.3" "greenlet==1.0.0" "gevent==21.1.2"
 if [ $? -ne 0 ]; then
     exit 1
 fi
