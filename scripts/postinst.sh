4f7470cc323883cc5adf7fc52946c6785e248c05
tang <tanguy.bonneau@gmail.com>
2024-03-06 18:14:23 +0100

    Fix dependencies after python3.9 update

--- scripts/postinst.sh
+++ scripts/postinst.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # install libs
-python3 -m pip install https://github.com/CleepDevice/cleep-libs-prebuild/raw/main/pyzmq/pyzmq-22.3.0-cp37-cp37m-linux_armv7l.whl
+python3 -m pip install https://github.com/CleepDevice/cleep-libs-prebuild/raw/main/pyzmq/bullseye/pyzmq-22.3.0-cp39-cp39-linux_armv7l.whl
 python3 -m pip install --trusted-host pypi.org "pyre-gevent==0.2.3"
 if [ $? -ne 0 ]; then
     exit 1
